8f2a6fd56c600a026da1ccc2cc58f9872f3bc487
joshiejack <joshjackwildman@gmail.com>
2014-01-24 04:15:32 +0000

    Fixed T2 Recipe

--- src/mariculture/plugins/PluginForestry.java
+++ src/mariculture/plugins/PluginForestry.java
@@ -99,6 +99,7 @@ public class PluginForestry extends Plugin {
 								Character.valueOf('C'), Block.chest }));
 
 				ItemStack silk = ItemInterface.getItem("craftingMaterial");
+				silk.setItemDamage(4);
 
 				RecipeManagers.carpenterManager.addRecipe(200,
 						FluidRegistry.getFluidStack("water", 1000),
