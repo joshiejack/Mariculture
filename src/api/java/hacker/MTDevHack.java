a98822bd2ec4bfa652b56c1ccf3b902617e578d0
joshiejack <joshjackwildman@gmail.com>
2014-07-18 23:05:34 +0100

    ... Fix broken shit in api folder.

--- src/api/java/hacker/MTDevHack.java
+++ src/api/java/hacker/MTDevHack.java
@@ -3,15 +3,10 @@ package hacker;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
-import minetweaker.MineTweakerAPI;
-import minetweaker.mc1710.brackets.ItemBracketHandler;
-import minetweaker.mc1710.brackets.LiquidBracketHandler;
-import minetweaker.mc1710.brackets.OreBracketHandler;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.tileentity.TileEntity;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "aMTDevHack", name = "MTDevHack", dependencies = "required-after:MineTweaker3")
@@ -36,12 +31,5 @@ public class MTDevHack {
         if (REMOVE_FML_ANNOYANCE) {
             setLogger(TileEntity.class);
         }
-    }
-
-    @EventHandler
-    public void init(FMLPostInitializationEvent event) {
-        MineTweakerAPI.registerBracketHandler(new ItemBracketHandler());
-        MineTweakerAPI.registerBracketHandler(new LiquidBracketHandler());
-        MineTweakerAPI.registerBracketHandler(new OreBracketHandler());
     }
 }
