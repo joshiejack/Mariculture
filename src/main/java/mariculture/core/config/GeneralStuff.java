98be76f2efb38f74813628cf86d9d6aded09fb62
TehSomeLuigi <olivier.the.olive+gh@gmail.com>
2014-07-24 18:56:23 +0100

    Add Config Option for Vertical Speedboat Modifier

--- src/main/java/mariculture/core/config/GeneralStuff.java
+++ src/main/java/mariculture/core/config/GeneralStuff.java
@@ -15,6 +15,7 @@ public class GeneralStuff {
     public static int METAL_RATE;
     public static boolean ENABLE_ENDER_SPAWN;
     public static boolean SHOW_CASTER_RECIPES;
+    public static double SPEEDBOAT_VERTICAL_MODIFIER;
 
     public static void init(Configuration config) {
         setConfig(config);
@@ -30,5 +31,6 @@ public class GeneralStuff {
         FLUDD_WATER_ON = getBoolean("Enable FLUDD Animations", true, "Whether a server will tell the client to display the fludd animations");
         ENABLE_ENDER_SPAWN = getBoolean("Enable Ender Dragon Spawning", true, "This is whether players can Spawn the Ender Dragon with the Dragon Spawn Egg");
         SHOW_CASTER_RECIPES = getBoolean("Enable Casting Recipes in NEI", false);
+        SPEEDBOAT_VERTICAL_MODIFIER = getDouble("Speedboat Vertical Modifier", 2.0D, "This changes the speed modifier of a speedboat moving upwards in water when the speedboat is at least 90% covered in water.");
     }
 }
