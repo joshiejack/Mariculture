f8b80047c3938a396d187158f3dcf57994ddb51d
joshiejack <joshjackwildman@gmail.com>
2014-07-31 23:39:25 +0100

    Electrified goo lightning effect is now on update instead of on collide.

--- src/main/java/mariculture/fishery/blocks/fluids/BlockFlux.java
+++ src/main/java/mariculture/fishery/blocks/fluids/BlockFlux.java
@@ -1,9 +1,9 @@
 package mariculture.fishery.blocks.fluids;
 
+import java.util.Random;
+
 import mariculture.core.blocks.base.BlockFluid;
 import net.minecraft.block.material.Material;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.Fluid;
@@ -16,9 +16,11 @@ public class BlockFlux extends BlockFluid {
     }
 
     @Override
-    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
-        if (entity instanceof EntityLivingBase && !world.isRemote && world.rand.nextInt(64) == 0) {
+    public void updateTick(World world, int x, int y, int z, Random rand) {
+        if (rand.nextInt(8) == 0) {
             world.addWeatherEffect(new EntityLightningBolt(world, x + world.rand.nextInt(5) - world.rand.nextInt(10), y, z + world.rand.nextInt(5) - world.rand.nextInt(10)));
         }
+
+        super.updateTick(world, x, y, z, rand);
     }
 }
