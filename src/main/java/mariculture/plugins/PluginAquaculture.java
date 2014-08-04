0392470630ee164edcf716c841c0a92c0f2609e2
joshiejack <joshjackwildman@gmail.com>
2014-08-04 17:26:04 +0100

    Add check for fishery module with harvestcraft and aquaculture plugins.

--- src/main/java/mariculture/plugins/PluginAquaculture.java
+++ src/main/java/mariculture/plugins/PluginAquaculture.java
@@ -3,13 +3,17 @@ package mariculture.plugins;
 import static mariculture.core.helpers.RecipeHelper.addShapeless;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
+import mariculture.api.fishery.Fishing;
+import mariculture.api.fishery.RodType;
 import mariculture.api.fishery.fish.FishSpecies;
 import mariculture.core.Core;
 import mariculture.core.lib.MaterialsMeta;
+import mariculture.core.lib.Modules;
 import mariculture.plugins.Plugins.Plugin;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -29,18 +33,23 @@ public class PluginAquaculture extends Plugin {
 
     @Override
     public void init() {
-        return;
+        if (Modules.isActive(Modules.fishery)) {
+            Fishing.fishing.addBait(new ItemStack(AquacultureItems.fish, 1, 19), 75);
+            Fishing.fishing.addBaitForQuality(new ItemStack(AquacultureItems.fish, 1, 19), Arrays.asList(RodType.DIRE, RodType.FLUX));
+        }
     }
 
     @Override
     public void postInit() {
-        //Remove Fish Fillet Recipes, We readd them later with a new recipe
-        remove(getItem("item.loot"), 3);
+        if (Modules.isActive(Modules.fishery)) {
+            //Remove Fish Fillet Recipes, We readd them later with a new recipe
+            remove(getItem("item.loot"), 3);
 
-        for (Entry<Integer, FishSpecies> list : FishSpecies.species.entrySet()) {
-            FishSpecies species = list.getValue();
-            if (species.getFishMealSize() > 0) {
-                addShapeless(new ItemStack(getItem("item.loot"), species.getFishMealSize(), 3), new Object[] { species.getRawForm(1), "foodSalt" });
+            for (Entry<Integer, FishSpecies> list : FishSpecies.species.entrySet()) {
+                FishSpecies species = list.getValue();
+                if (species.getFishMealSize() > 0) {
+                    addShapeless(new ItemStack(getItem("item.loot"), species.getFishMealSize(), 3), new Object[] { species.getRawForm(1), "foodSalt" });
+                }
             }
         }
     }
--- src/main/java/mariculture/plugins/PluginHarvestcraft.java
+++ src/main/java/mariculture/plugins/PluginHarvestcraft.java
@@ -2,6 +2,7 @@ package mariculture.plugins;
 
 import static mariculture.core.helpers.RecipeHelper._;
 import static mariculture.core.helpers.RecipeHelper.addShaped;
+import mariculture.core.lib.Modules;
 import mariculture.plugins.Plugins.Plugin;
 
 /** This class is to add my fish to pams fish recipes **/
@@ -17,12 +18,14 @@ public class PluginHarvestcraft extends Plugin {
 
     @Override
     public void init() {
-        addShaped(_(getItem("fishsandwichItem")), new Object[] { "toolSkillet", "fish", "foodMayo" });
-        addShaped(_(getItem("fishsticksItem")), new Object[] { "toolBakeware", "fish", "foodFlour" });
-        addShaped(_(getItem("stockItem")), new Object[] { "toolPot", "fish", "toolMixingbowl" });
-        addShaped(_(getItem("fishdinnerItem")), new Object[] { "toolSkillet", "cropLemon", "foodFlour", "fish", "foodMayo" });
-        addShaped(_(getItem("sushiItem")), new Object[] { "toolCuttingboard", "fish", "cropRice" });
-        addShaped(_(getItem("fishtacoItem")), new Object[] { "toolCuttingboard", "cropLettuce", "fish" });
+        if (Modules.isActive(Modules.fishery)) {
+            addShaped(_(getItem("fishsandwichItem")), new Object[] { "toolSkillet", "fish", "foodMayo" });
+            addShaped(_(getItem("fishsticksItem")), new Object[] { "toolBakeware", "fish", "foodFlour" });
+            addShaped(_(getItem("stockItem")), new Object[] { "toolPot", "fish", "toolMixingbowl" });
+            addShaped(_(getItem("fishdinnerItem")), new Object[] { "toolSkillet", "cropLemon", "foodFlour", "fish", "foodMayo" });
+            addShaped(_(getItem("sushiItem")), new Object[] { "toolCuttingboard", "fish", "cropRice" });
+            addShaped(_(getItem("fishtacoItem")), new Object[] { "toolCuttingboard", "cropLettuce", "fish" });
+        }
     }
 
     @Override
