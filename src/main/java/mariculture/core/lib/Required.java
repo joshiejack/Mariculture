631fc335e1063486b54758c18b0dc0bf96bc2d1d
joshiejack <joshjackwildman@gmail.com>
2014-08-12 18:31:46 +0100

    Fix load order issue with HEE Plugin

--- src/main/java/mariculture/core/lib/Required.java
+++ src/main/java/mariculture/core/lib/Required.java
@@ -5,5 +5,6 @@ public class Required {
     private static final String enchiridion = "1.1";
     private static final String blood_magic = "v1.0.1";
     private static final String bop = "2.0.0";
-    public static final String after = "required-after:Forge@[" + forge + ",);required-after:Enchiridion@[" + enchiridion + ",);" + "before:Mariculture|API;" + "after:AWWayofTime@[" + blood_magic + ",);after:BiomesOPlenty@[" + bop + ",);after:TConstruct;after:ThermalFoundation";
+    private static final String liquid_after = "after:HardcoreEnderExpansion;after:TConstruct;after:ThermalFoundation";
+    public static final String after = "required-after:Forge@[" + forge + ",);required-after:Enchiridion@[" + enchiridion + ",);" + "before:Mariculture|API;" + "after:AWWayofTime@[" + blood_magic + ",);after:BiomesOPlenty@[" + bop + ",);" + liquid_after;
 }
