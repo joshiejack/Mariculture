e41dcb411d34c8b120e1806e335f9741017ed578
joshiejack <joshjackwildman@gmail.com>
2014-08-19 21:53:05 +0100

    Anvil will no longer allow repair if the cost of repair is 0. Fixes #169

--- src/main/java/mariculture/core/tile/TileAnvil.java
+++ src/main/java/mariculture/core/tile/TileAnvil.java
@@ -82,7 +82,7 @@ public class TileAnvil extends TileStorage implements ISidedInventory, IAnvilHan
     }
 
     private boolean hasXP(EntityPlayer player, float xp) {
-        return player.experience >= (xp * 0.05882353F) || player.experienceLevel > 0;
+        return xp > 0 && (player.experience >= (xp * 0.05882353F) || player.experienceLevel > 0);
     }
 
     public int workItem(EntityPlayer player, ItemStack hammer) {
