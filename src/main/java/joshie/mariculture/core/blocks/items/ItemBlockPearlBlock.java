432402f35df4217c3f16cae22387303721c4ee5c
joshiejack <joshjackwildman@gmail.com>
2014-09-05 10:12:58 +0100

    Start moving translations over, Update build.gradle

--- src/main/java/joshie/mariculture/core/blocks/items/ItemBlockPearlBlock.java
+++ src/main/java/joshie/mariculture/core/blocks/items/ItemBlockPearlBlock.java
@@ -2,6 +2,8 @@ package joshie.mariculture.core.blocks.items;
 
 import joshie.mariculture.core.blocks.base.ItemBlockMariculture;
 import joshie.mariculture.core.lib.PearlColor;
+import joshie.mariculture.core.lib.UpgradeMeta;
+import joshie.mariculture.core.util.MCTranslate;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 
@@ -13,5 +15,16 @@ public class ItemBlockPearlBlock extends ItemBlockMariculture {
     @Override
     public String getName(ItemStack stack) {
         return PearlColor.get(stack.getItemDamage());
+    }
+    
+    @Override
+    public String getItemStackDisplayName(ItemStack stack) {
+        String format = MCTranslate.translate("pearl.format");
+        format = format.replace("%C", MCTranslate.translate("pearl.color." + PearlColor.get(stack.getItemDamage())));
+        format = format.replace("%P", MCTranslate.translate("pearl"));
+        String unlocalized = field_150939_a.getUnlocalizedName();
+        if(unlocalized.contains("block")) {
+            return format.replace("%B", MCTranslate.translate("pearl.block"));
+        } else return format.replace("%B", MCTranslate.translate("pearl.brick"));
     }
 }
