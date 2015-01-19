c50199512a0defa83f5f350fe01806dba2683684
joshiejack <joshjackwildman@gmail.com>
2015-01-19 22:37:57 +0000

    Add PenguinCore dependency.

--- src/main/java/joshie/mariculture/lib/MaricultureInfo.java
+++ src/main/java/joshie/mariculture/lib/MaricultureInfo.java
@@ -5,9 +5,10 @@ public class MaricultureInfo {
 	public static final String MODID = "Mariculture2";
 	public static final String MODNAME = "Mariculture 2";
 	public static final String MODPATH = "mariculture";
-	public static final String DEPENDENCIES = "";
     public static final String INITIALS = "M";
+    public static final String PENGUINCORE_VERSION = "2.0.0";
     public static final String VERSION = "@VERSION@";
+	public static final String DEPENDENCIES = "required-after:PenguinCore@[" + PENGUINCORE_VERSION + ",)";
     
     public static final String[] MODULES = new String[] { 
     	"diving", "exploration", "fishery", "sealife"
