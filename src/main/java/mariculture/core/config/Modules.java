fee36655a2b67cba6341ad05829c6d383a329d2c
joshiejack <joshjackwildman@gmail.com>
2015-04-19 05:33:44 +0100

    Nah.

--- src/main/java/mariculture/core/config/Modules.java
+++ src/main/java/mariculture/core/config/Modules.java
@@ -6,7 +6,6 @@ import mariculture.core.Core;
 import mariculture.diving.Diving;
 import mariculture.factory.Factory;
 import mariculture.fishery.Fishery;
-import mariculture.lost.Lost;
 import mariculture.magic.Magic;
 import mariculture.plugins.Plugins;
 import mariculture.plugins.Plugins.Plugin;
@@ -17,7 +16,6 @@ import net.minecraftforge.common.config.Configuration;
 public class Modules {
     public static void init(Configuration config) {
         Mariculture.modules.setup(Core.class, true);
-        Mariculture.modules.setup(Lost.class, config.get("Modules", "Alpha Mariculture", true).getBoolean(true));
         Mariculture.modules.setup(Aesthetics.class, config.get("Modules", "Aesthetics", true).getBoolean(true));
         Mariculture.modules.setup(Diving.class, config.get("Modules", "Diving", true).getBoolean(true));
         Mariculture.modules.setup(Factory.class, config.get("Modules", "Factory", true).getBoolean(true));
--- src/main/java/mariculture/core/lib/Modules.java
+++ src/main/java/mariculture/core/lib/Modules.java
@@ -21,7 +21,6 @@ public class Modules {
     public static Module diving;
     public static Module factory;
     public static Module fishery;
-    public static Module lost;
     public static Module magic;
     public static Module sealife;
     public static Module transport;
